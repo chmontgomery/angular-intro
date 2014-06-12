a5517f3113f4a7bf296c6f663229789a878d6ef0
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-06-12 12:01:12 -0500

    step two

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -6,10 +6,23 @@
     .controller('mainController',
     ['$scope', 'dylanService',
       function ($scope, dylanService) {
+        $scope.instruments = [];
         $scope.msg = 'Hello, World!';
-        dylanService.getQuotes()
+        dylanService.getQuotes('MSFT|US|||,AAPL|US|||')
           .then(function(data) {
             $scope.instruments = data.GetInstrumentResponse.InstrumentResponses;
           });
+        $scope.addInstrument = function(ticker) {
+          dylanService.getQuotes(ticker + '|US|||')
+            .then(function(data) {
+              console.log(data.GetInstrumentResponse.InstrumentResponses[0]);
+              $scope.instruments.push(data.GetInstrumentResponse.InstrumentResponses[0]);
+            });
+        };
+        $scope.ticker = '';
+        $scope.addTicker = function() {
+          $scope.addInstrument($scope.ticker);
+          $scope.ticker = '';
+        };
       }]);
 })();
--- content/js/services.js
+++ content/js/services.js
@@ -5,11 +5,11 @@
   angular.module('newsApp.services', [])
     .factory('dylanService', ['$q', '$http', function ($q, $http) {
       return {
-        getQuotes: function () {
+        getQuotes: function (tickers) {
           var deferred = $q.defer();
           $http({
             method: 'GET',
-            url: 'http://api.wsj.net/api/dylan/quotes/v2/comp/quote?id=MSFT|US|||,AAPL|US|||&needed=CompositeTrading|Financials|BlueGrassChannels&MaxInstrumentMatches=1&ckey=cecc4267a0',
+            url: 'http://api.wsj.net/api/dylan/quotes/v2/comp/quote?id=' + tickers + '&needed=CompositeTrading|Financials|BlueGrassChannels&MaxInstrumentMatches=1&ckey=cecc4267a0',
             headers: {
               'Accept': 'application/json',
               'Dylan2010.EntitlementToken': 'cecc4267a0194af89ca343805a3e57af'
